6431c19d6750b994093e5a3e643a309e90112933
Riffatk <kriffat694@gmail.com>
2026-04-26 07:59:29 +0530

    Added to do list project

---
+++ to_do_list.py
@@ -0,0 +1,34 @@
+tasks = []
+
+while True:
+    print("\n1. Add Task")
+    print("2. View Tasks")
+    print("3. Remove Task")
+    print("4. Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        task = input("Enter task: ")
+        tasks.append(task)
+        print("Task added!")
+
+    elif choice == "2":
+        print("Your Tasks:")
+        for task in tasks:
+            print("-", task)
+
+    elif choice == "3":
+        task = input("Enter task to remove: ")
+        if task in tasks:
+            tasks.remove(task)
+            print("Task removed!")
+        else:
+            print("Task not found.")
+
+    elif choice == "4":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid choice")
